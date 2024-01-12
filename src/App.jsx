c997b03db2d7415723b414628d153e226d1b84d9
Meet Chanchad <123048686+meetchanchad@users.noreply.github.com>
2024-01-12 10:25:36 +0530

    ui complite

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,33 @@ import './App.css'
 function App() {
 
   return (
-    <>
-      <h1>Password Generator React</h1>
-    </>
+    <div id='main'>
+
+      <div className='pass-gen-main'>
+
+        <div className="pass-gen-wrapper">
+
+          <div className='pass-gen-txt'>
+            <h1>Password Generator</h1>
+          </div>
+
+          <div className='pass-gen-input'>
+            <div><input type="text" readOnly /><button>Copy</button></div>
+          </div>
+
+          <div className='pass-gen-length'>
+            <div className='range'><input type="range" id='range' /><label htmlFor="range">length</label></div>
+            <div className='checkbox'>
+              <div><input type="checkbox" id='numbers' /><label htmlFor="numbers">Numbers</label></div>
+              <div><input type="checkbox" id='char' /><label htmlFor="char">Charactors</label></div>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
   )
 }
 
