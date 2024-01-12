ef34145b93d20f04850332a85e21031093f613eb
Meet Chanchad <123048686+meetchanchad@users.noreply.github.com>
2024-01-12 11:49:57 +0530

    hooks - useState, useCallback, useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
-import { useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
+
+  const [password, setPassword] = useState('')
+  const [length, setLength] = useState(7)
+  const [numberAllowed, setNumberAllowed] = useState(false)
+  const [charAllowed, setCharAllowed] = useState(false)
+
+
+  const passwordGeneretor = useCallback(() => {
+
+  }, [length, numberAllowed, charAllowed, setPassword])
+
+  useEffect(() => {
+
+  }, [length, numberAllowed, charAllowed, passwordGeneretor])
+
+
 
   return (
     <div id='main'>
@@ -15,14 +31,14 @@ function App() {
           </div>
 
           <div className='pass-gen-input'>
-            <div><input type="text" readOnly /><button>Copy</button></div>
+            <div><input type="text" readOnly value={password} /><button>Copy</button></div>
           </div>
 
           <div className='pass-gen-length'>
-            <div className='range'><input type="range" id='range' /><label htmlFor="range">length</label></div>
+            <div className='range'><input value={length} type="range" id='range' min={7} max={50} onChange={(e) => { setLength(e.target.value) }} /><label htmlFor="range">length: {length}</label></div>
             <div className='checkbox'>
-              <div><input type="checkbox" id='numbers' /><label htmlFor="numbers">Numbers</label></div>
-              <div><input type="checkbox" id='char' /><label htmlFor="char">Charactors</label></div>
+              <div><input type="checkbox" id='numbers' defaultChecked={numberAllowed} onChange={() => { setNumberAllowed((prev) => !prev) }} /><label htmlFor="numbers">Numbers</label></div>
+              <div><input type="checkbox" id='char' defaultChecked={charAllowed} onChange={() => { setCharAllowed((prev) => !prev) }} /><label htmlFor="char" >Charactors</label></div>
             </div>
           </div>
 
